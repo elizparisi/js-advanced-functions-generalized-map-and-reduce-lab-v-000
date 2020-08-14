37684d365eeba32ff8ebb96a4ec1b84620c1d829
Liz P <parisielizabeth@gmail.com>
2020-08-14 05:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function map(array, func){
 
 function reduce(array, func, start){
   let result = (!!start) ? start : array[0];
-  for (let i = 0 : 1; i < array.length; i++){
+  let i = (!!start) ? 0 : 1;
+  for (; i < array.length; i++){
     result = func(array[i], result);
   }
   return result;
