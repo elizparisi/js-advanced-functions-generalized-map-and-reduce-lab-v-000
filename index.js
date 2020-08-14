3d62a5e318772dc99ef15a6cd215210a74f09259
Liz P <parisielizabeth@gmail.com>
2020-08-14 04:51:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Add your functions here
 
 function map(array, func){
-  result = [];
+  let result = [];
   for (let i = 0; i < array.length; i++){
     result.push(func(array[i]))
   }
   return result;
 }
+
